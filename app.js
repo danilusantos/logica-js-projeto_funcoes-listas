8f6bb692600d403c530d9ca8e2d1a50f7b497c81
Danilo Santos <danilondosantos@gmail.com>
2024-06-21 16:44:34 -0300

    feat: :sparkles: using responsive voice in application

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
-    campo.innerHTML = texto
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {
+        rate: 1.2
+    });
 }
 
 function exibirMensagemInicial() {
