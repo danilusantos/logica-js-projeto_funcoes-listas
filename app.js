ede7dbea2443a8eb74a86ec22944e526cbde835a
Danilo Santos <danilondosantos@gmail.com>
2024-06-21 15:41:44 -0300

    feat: :sparkles: restarting the game

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -38,4 +42,12 @@ function gerarNumeroAleatorio() {
 function limparCampo() {
     let chute = document.querySelector('input');
     chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio;
+    limparCampo();
+    tentativas = 1;
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+    exibirMensagemInicial();
 }
