8a3f13c28f41d7bb5cbcf193ae7c6f2c4cf14a9a
Danilo Santos <danilondosantos@gmail.com>
2024-06-21 14:31:30 -0300

    feat: :sparkles: verify try

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -10,7 +10,17 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+
+    if (chute != numeroSecreto) {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor.');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior.');
+        }
+    } else {
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', 'Você descobriu o número secreto.');
+    }
 }
 
 function gerarNumeroAleatorio() {
